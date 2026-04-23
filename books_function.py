634ca586d5a05956620dc9b3894b852ae5adc733
Andrii_Potapov <Andrijpotapov1@gmail.com>
2026-04-23 22:01:17 +0200

    debug

--- books_function.py
+++ books_function.py
@@ -2,7 +2,7 @@ book = []
 
 def add_book():
     name_user = str(input("book name: \n" ))
-    autor = int(input("autor book : \n"))
+    autor = str(input("autor book : \n"))
     price = int(input("price : \n"))
 
     
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def add_book():
-    b = []
-    us = input("fgfdv ")
-    b.append(us)
-    print(b)
+from books_function import add_book, show_books
 
 system = True
 while system:
-    user_choice = input("Додати книгу - 1 \n оказати всі книги - 2\n Вийти - 3\n").strip()
+    user_choice = input("Додати книгу - 1 \n Показати всі книги - 2\n Вийти - 3\n").strip()
     if user_choice == "1":
         add_book()  
     elif user_choice == "2":
@@ -15,3 +11,4 @@ while system:
         system = False
     else:
         print("Введіть число яке є в списку команд!")
+
