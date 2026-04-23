d46a7c8796fcbaaceebedcbb8b636a8a242aee51
osFERPHY <vadumlavriv562@gmail.com>
2026-04-23 19:57:22 +0300

    topvadum

--- gui.py
+++ gui.py
@@ -1,1 +1,26 @@
-import pyside6
+book = []
+
+def add_book():
+    name_user = str(input("book name: \n" ))
+    autor = int(input("autor book : \n"))
+    price = int(input("price : \n"))
+
+    
+   # newbook.append (f"{name_user} {autor} {price} \n")
+    newbook = {
+        "title": name_user,
+        "autor": autor,
+        "price": price
+    }
+    book.append(newbook)
+
+def show_books():
+    for i in book:
+        print(i) 
+
+
+
+
+
+
+
